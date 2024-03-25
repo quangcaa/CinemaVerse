15f48e49e516658a032590bbcbae02c40c4f8493
Maou <nguyenvietquang.1601@gmail.com>
2024-03-25 22:30:34 +0700

    #1 - fix

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -5,14 +5,14 @@ const mongoUri = `mongodb+srv://quangcaa:${process.env.DB_PASSWORD}@quangcaa.9ft
 
 async function connect() {
     try {
-        mongoose.set('strictQuery', false);
+        mongoose.set('strictQuery', false)
         await mongoose.connect(mongoUri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB Connected ...');
+        console.log('MongoDB Connected ... :')
     } catch (error) {
-        console.log('Connect failure !!!');
+        console.log(`Connect failure !!!\n${error.message}`)
     }
 }
 
