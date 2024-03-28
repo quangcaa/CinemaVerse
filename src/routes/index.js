a0a9e5d0b80f9425542f9de4ecb888ea8bd8b14c
Maou <nguyenvietquang.1601@gmail.com>
2024-03-28 23:32:12 +0700

    #6 - movie db update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 const browseRouter = require('./browse')
+const adminRouter = require('./admin')
+
 
 function route(app) {
     app.use('/', browseRouter)
+    app.use('/admin', adminRouter)
+
 }
 
 module.exports = route
